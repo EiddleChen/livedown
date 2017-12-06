923baa53821ae57ff8aba84835276ddccb62ca26
yitong.cyt <yitong.cyt@alipay.com>
2017-12-06 19:16:26 +0800

    Support puml code

---
+++ public/puml.js
@@ -0,0 +1,51 @@
+var socket = io.connect(window.location.origin)
+
+function encode64(data) {
+  for (var r = '', i = 0, n = data.length; i < n; i += 3)
+    r += append3bytes(
+      data.charCodeAt(i),
+      i + 1 != n ? data.charCodeAt(i + 1) : 0,
+      i + 2 != n ? data.charCodeAt(i + 2) : 0);
+  return r;
+}
+
+function append3bytes(b1, b2, b3) {
+  var c1 = b1 >> 2;
+  var c2 = ((b1 & 0x3) << 4) | (b2 >> 4);
+  var c3 = ((b2 & 0xF) << 2) | (b3 >> 6);
+  var c4 = b3 & 0x3F;
+  var r =
+    encode6bit(c1 & 0x3F) +
+    encode6bit(c2 & 0x3F) +
+    encode6bit(c3 & 0x3F) +
+    encode6bit(c4 & 0x3F);
+  return r;
+}
+
+function encode6bit(b) {
+  if (b < 10)
+    return String.fromCharCode(48 + b);
+  b -= 10;
+  if (b < 26)
+    return String.fromCharCode(65 + b);
+  b -= 26;
+  if (b < 26)
+    return String.fromCharCode(97 + b);
+  b -= 26;
+  if (b == 0)
+    return '-';
+  if (b == 1)
+    return '_';
+  return '?';
+}
+
+socket.on('content', function(data) {
+  $('pre.lang-puml').each(function(index, block) {
+    var code = block.textContent;
+    var s = unescape(encodeURIComponent(code));
+    var url = 'http://www.plantuml.com/plantuml/img/' + encode64(deflate(s));
+    var img = new Image();
+    img.src = url;
+    block.replaceWith(img);
+  });
+})
